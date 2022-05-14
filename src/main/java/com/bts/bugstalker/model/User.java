791468286170ae987b666bd90e34b55844dbb04f
Adam Kalisz <kaliszadam99@gmail.com>
2022-05-14 12:22:58 +0200

    Revert "BSA-36 project structure refactor, added mapper and exception handling (#1)" (#2)
    This reverts commit f64e35308265580c7a8397ac4f952b33ba89b5e8.

--- src/main/java/com/bts/bugstalker/model/User.java
+++ src/main/java/com/bts/bugstalker/model/User.java
@@ -1,33 +1,26 @@
-package com.bts.bugstalker.core.user;
+package com.bts.bugstalker.model;
 
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-class User {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String username;
-
     private String firstName;
-
     private String lastName;
-
-    @NotBlank
     private String email;
-
     private String password;
-
     private String salt;
 
 }
---
+++ src/main/java/com/bts/bugstalker/util/property/AppContextProvider.java
@@ -0,0 +1,7 @@
+package com.bts.bugstalker.util.property;
+
+public final class AppContextProvider {
+
+    public static final String API_V1_PATH = "/api/v1";
+
+}
