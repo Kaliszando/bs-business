a4a922382d81612c476fce56803c3400125533bf
Kaliszando <kaliszadam99@gmail.com>
2022-05-14 12:15:02 +0200

    BSA-36 project structure refactor, added mapper and exception handling

--- src/main/java/com/bts/bugstalker/core/user/UserRepository.java
+++ src/main/java/com/bts/bugstalker/core/user/UserRepository.java
@@ -1,13 +1,12 @@
-package com.bts.bugstalker.repository;
+package com.bts.bugstalker.core.user;
 
-import com.bts.bugstalker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
---
+++ src/main/java/com/bts/bugstalker/util/context/AppInfoProvider.java
@@ -0,0 +1,7 @@
+package com.bts.bugstalker.util.context;
+
+public final class AppInfoProvider {
+
+    public static final String API_V1_PATH = "/api/v1";
+
+}
