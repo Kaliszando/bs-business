6f0cca9b40555403c26381f7f2c95d36969520c3
Adam Kalisz <kaliszadam99@gmail.com>
2024-10-15 19:01:02 +0200

    BS-72 added Testcontainers for Redis server (#98)

--- src/test/java/com/bts/bugstalker/config/cache/RedisMockConfig.java
+++ src/test/java/com/bts/bugstalker/config/cache/RedisMockConfig.java
@@ -1,65 +1,27 @@
 package com.bts.bugstalker.config.cache;
 
 import com.bts.bugstalker.util.properties.RedisProperties;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import com.redis.testcontainers.RedisContainer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PreDestroy;
-import java.io.IOException;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Slf4j
+@Testcontainers
 @TestConfiguration
-@RequiredArgsConstructor
 public class RedisMockConfig {
 
-    @Autowired
-    private LettuceConnectionFactory lettuceConnectionFactory;
-
-    private RedisServer redisServer;
-
-    @Bean
-    public RedisServer redisServer(RedisProperties redisProperties) throws IOException {
-        redisServer = new RedisServer(redisProperties.getRedisPort());
-
-        log.info("Starting embedded redis server on port: {}", redisProperties.getRedisPort());
-        redisServer.start();
-        return redisServer;
-    }
+    @Container
+    public static RedisContainer redisContainer = new RedisContainer("redis:6.2.1");
 
     @Bean
     @Primary
-    public LettuceConnectionFactory lettuceConnectionFactoryNoPassword(RedisProperties redisProperties,
-                                                                       LettuceClientConfiguration clientConfiguration) {
-        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(
-                redisProperties.getRedisHost(), redisProperties.getRedisPort());
-        return new LettuceConnectionFactory(serverConfig, clientConfiguration);
-    }
-
-    @PreDestroy
-    public void preDestroy() throws IOException {
-        redisServer.stop();
-    }
-
-    @SneakyThrows
-    @EventListener(ContextClosedEvent.class)
-    public void stopRedis() {
-        if (lettuceConnectionFactory != null) {
-            lettuceConnectionFactory.destroy();
-        }
-
-        if (redisServer != null) {
-            redisServer.stop();
-        }
+    public RedisProperties redisPropertiesMock() {
+        redisContainer.start();
+        log.info("Redis container started on port: {}", redisContainer.getFirstMappedPort());
+        return new RedisProperties(redisContainer.getRedisPort(), redisContainer.getHost(), "password");
     }
 }
