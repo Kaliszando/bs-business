9bf3ced2eb1f1a8bca4e0a5bcdfc5f09faf06ade
Adam Kalisz <adam.kalisz@comarch.com>
2024-10-31 17:28:36 +0100

    BS-101 BE replaced Lettuce with Jedis as Redis client

--- src/main/java/com/bts/bugstalker/config/cache/RedisConfig.java
+++ src/main/java/com/bts/bugstalker/config/cache/RedisConfig.java
@@ -2,19 +2,19 @@ package com.bts.bugstalker.config.cache;
 
 import com.bts.bugstalker.feature.cache.jwt.JwtCache;
 import com.bts.bugstalker.util.properties.RedisProperties;
-import io.lettuce.core.ClientOptions;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 @EnableAutoConfiguration(exclude = RedisRepositoriesAutoConfiguration.class)
 @EnableRedisRepositories(basePackageClasses = JwtCache.class)
@@ -23,24 +23,32 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public LettuceClientConfiguration lettuceClientConfiguration() {
-        return LettuceClientConfiguration.builder()
-                .clientOptions(ClientOptions.builder()
-                        .autoReconnect(false)
-                        .build())
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(0);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        return poolConfig;
+    }
+
+    @Bean
+    public JedisClientConfiguration jedisClientConfiguration(JedisPoolConfig poolConfig) {
+        return JedisClientConfiguration.builder()
+                .usePooling()
+                .poolConfig(poolConfig)
                 .build();
     }
 
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties, LettuceClientConfiguration clientConfiguration) {
+    public JedisConnectionFactory jedisConnectionFactory(RedisProperties redisProperties, JedisClientConfiguration clientConfiguration) {
         RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(
                 redisProperties.getRedisHost(), redisProperties.getRedisPort());
-        serverConfig.setPassword(redisProperties.getRedisPassword());
-        return new LettuceConnectionFactory(serverConfig, clientConfiguration);
+        return new JedisConnectionFactory(serverConfig, clientConfiguration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
